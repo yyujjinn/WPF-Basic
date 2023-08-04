a7bb4c33756529899fed5e0532c8d56224285e42
yyujjinn <yujin@ati2000.co.kr>
2023-08-04 16:41:31 +0900

    Hello Button

---
+++ ArduinoDE/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArduinoDE
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, RoutedEventArgs e)
+        {
+            tbHi.Text = btnHello.Content.ToString() + "World";
+        }
+    }
+}
